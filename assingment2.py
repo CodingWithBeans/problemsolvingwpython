03e6b596f9af6533d9f275cdea5945129f6a15d7
Matty Laptop <mattylee0121@gmail.com>
2025-11-13 15:12:59 +0000

    histogram now shows, very unclean

--- assingment2.py
+++ assingment2.py
@@ -1,10 +1,14 @@
 import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
 
 data = pd.read_csv('kpindex.csv')
+df = pd.DataFrame(data)
 print(data.count()) #271016
-
+print(len(data)) #271016
 print(data.max("columns" == "KP"))
 print(data.describe())
 
-
-
+plottingdata = df["KP"]
+plot = plottingdata.plot.hist()
+plt.show()
