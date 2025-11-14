51b9f1fdae75d271b0a5c3a5cdc574a00449bc80
Matty Laptop <mattylee0121@gmail.com>
2025-11-14 15:38:38 +0000

    histogram basic done

--- assingment2.py
+++ assingment2.py
@@ -2,13 +2,35 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
+
 data = pd.read_csv('kpindex.csv')
 df = pd.DataFrame(data)
 print(data.count()) #271016
 print(len(data)) #271016
 print(data.max("columns" == "KP"))
-print(data.describe())
-
+print("---" * 20)
 plottingdata = df["KP"]
-plot = plottingdata.plot.hist()
-plt.show()
+
+print(data.describe(), plottingdata.describe())
+
+   
+count, bins, patches = plt.hist(plottingdata, bins=28)
+colormap = plt.get_cmap('rainbow', len(patches))
+
+for i, patch in enumerate(patches):
+    patch.set_facecolor(colormap(i))
+#plt.show()
+
+years = np.arange(1970, 2025)
+print(years)
+#year_data = data[data['Year'] == year]
+# Assume that the variables data and years are already set
+
+# ADD CODE BELOW to initialise a list max_kps 
+max_kps = []
+
+# Loop over each year in years
+for y in years:
+
+    # ADD CODE BELOW to get the data for year y
+    # and add the max KP for y to max_kps
