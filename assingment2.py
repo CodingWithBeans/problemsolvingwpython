5d5f3352aa7b0c8e0226a5258061ab9f56d65649
beans <mattylee0121@gmail.com>
2025-11-19 09:41:27 +0000

    matty

--- assingment2.py
+++ assingment2.py
@@ -52,33 +52,41 @@ def secondGraph(max_kps, years):
 def significantYears(data):
 
     significant_years = {year for year in range(data['Year'].min(), data['Year'].max() + 1) if any(data.loc[data['Year'] == year, 'KP'] == data['KP'].max())}
-    print(significant_years, type(significant_years))
     return significant_years
 
-def createSingleDayHisto(date, data): 
-    # Get rows for this day
+def createSingleDayHisto(date, data):
+
+    #data stream
     day_data = data[(data['Year']==date[0]) &
                     (data['Month']==date[1]) &
                     (data['Day']==date[2])]
-    print(day_data)
+
     kps = day_data["KP"]
     hours = day_data["Period_Start"]
-    indexvcolours = {1:"#ff0000", 2:"#3cff00", 3:"#e100ff", 4:"#eeff00", 5:"#0044ff", 6:"#00e1ff", 7:"#ff5100", 0:"#ff009d",}
+    indexvcolours = {2:"#f700ff", 3:"#6200ff", 4:"#0004ff", 5:"#00d9ff", 6:"#00ff00", 7:"#fffb00", 8:"#ff6600", 9:"#ff0000",}
     
+    #initialise bar chart
+    plt.bar(hours, kps, width=3, align='center', color = [indexvcolours[round(kp)] for kp in kps], edgecolor="white", linewidth=0.5)
+    plt.title(f"The Kp index on {str(date)[1:-1]}")
 
-    print(hours, "hours", "-" * 20)
-    print(kps, "kps", "-" * 20)
-    plt.bar(hours, kps, width=3, align='edge', color = [indexvcolours[] for kp in kps])
-    plt.show()
-    plt.bar(hours, kps, width=3, align="center")
+
+    #X- axis
+    plt.xlabel("Hour")
+    plt.xticks(range(0, 25, 3))
+    plt.xlim(-2,24)
+
+    #Y - axis
+    plt.yticks(range(0, 10))
+    plt.ylim(0, max(kps) + 1)
+    plt.ylabel("KP")
     plt.show()
 
 def main():
     data, plottingdata, years = creatingVariables()
     max_kps = maxkpsCreation(data, years)
     printstatements(plottingdata, data)
-    firstGraph(plottingdata)
-    secondGraph(max_kps, years)
+    #firstGraph(plottingdata)
+    #secondGraph(max_kps, years)
     significantYears(data)
     dates = [[1972,8,5], [2024,5,11]]
     for date in dates:
