1690e2ab818f1c6dcac52e67997e46d9e38f9b1d
Matty Laptop <mattylee0121@gmail.com>
2025-11-19 13:44:13 +0000

    cleaned up graphs, removed ununsed imports, general spring cleaning, sorted out comments

--- assingment2.py
+++ assingment2.py
@@ -1,20 +1,21 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-from matplotlib import cm
+
 
 
 def creatingVariables():
+    #creating variables to be used later in the script
     data = pd.read_csv('kpindex.csv')
-    df = pd.DataFrame(data)
 
-    plottingdata = df["KP"]
+
+    plottingdata = data["KP"]
 
     years = np.arange(1970, 2025)
     return data, plottingdata, years
 
 def printstatements(plottingdata, data):
-
+    #some simple print statements for the first few questions
     print(data.describe(), plottingdata.describe())
     print(data.count()) #271016
     print(len(data)) #271016
@@ -22,12 +23,17 @@ def printstatements(plottingdata, data):
     print("---" * 20)
 
 def firstGraph(plottingdata):
-    #need to go nd add titles etc but basic set up is there
-    count, bins, patches = plt.hist(plottingdata, bins=28)
-    colormap = plt.get_cmap('rainbow', len(patches))
 
-    for i, patch in enumerate(patches):
-        patch.set_facecolor(colormap(i))
+    #creating the plot
+    plt.hist(plottingdata, bins=28, density=True, edgecolor="black")
+
+    #tried to add distribution curve, it looks nasty
+    #plottingdata.plot(kind='density', linewidth=2)
+
+    #labels etc
+    plt.title("Distribution of KP Index Values")
+    plt.xlabel("KP Value")
+    plt.ylabel("Density")
     plt.show()
 
 
@@ -45,16 +51,24 @@ def maxkpsCreation(data, years):
     return max_kps
 
 def secondGraph(max_kps, years):
-    #// line graph of years vs max kps go put titles and stuff on this matty haway
+    #line graph of kp index max per year
+
+    #creating plot
     plt.plot(years, max_kps)
+
+    #graph GUI output stuff
+    plt.title("Maximum Kp Index per Year")
+    plt.xlabel("Year")
+    plt.ylabel("Maximum Kp Value recorded within the year")
     plt.show()
 
 def significantYears(data):
-
+    #create significant years set, using set/list comprehension adding year to the set loops from the lowest year in the data to the max year + 1 as 0 based index
+    # if we find any year with a kps value == to the maximum permitted kpvalue then add to the set
     significant_years = {year for year in range(data['Year'].min(), data['Year'].max() + 1) if any(data.loc[data['Year'] == year, 'KP'] == data['KP'].max())}
     return significant_years
 
-def createSingleDayHisto(date, data):
+def createSingleDaybar(date, data):
 
     #data stream
     day_data = data[(data['Year']==date[0]) &
@@ -63,15 +77,21 @@ def createSingleDayHisto(date, data):
 
     kps = day_data["KP"]
     hours = day_data["Period_Start"]
-    indexvcolours = {2:"#f700ff", 3:"#6200ff", 4:"#0004ff", 5:"#00d9ff", 6:"#00ff00", 7:"#fffb00", 8:"#ff6600", 9:"#ff0000",}
+    indexvcolours = {1:"#000000",2:"#f700ff", 3:"#6200ff", 4:"#0004ff", 5:"#00d9ff", 6:"#00ff00", 7:"#fffb00", 8:"#ff6600", 9:"#ff0000",}
     
     #initialise bar chart
+
+    #here i initialise the bar chart with hours on the x axis, kps on the y axis, i aligned each chart in the center. for the colours i have set the rounded value of the kp for the time period using the dictionary indexvcolours as a colour map, edge colour and linewidth are to seperate each bar for visual reading
     plt.bar(hours, kps, width=3, align='center', color = [indexvcolours[round(kp)] for kp in kps], edgecolor="white", linewidth=0.5)
-    plt.title(f"The Kp index on {str(date)[1:-1]}")
+
+    #titles takes the date variable, converts to a string and slices the [] from the front and back
+    plt.title(f"The Kp Index on {str(date)[1:-1]}")
 
 
     #X- axis
     plt.xlabel("Hour")
+    
+    #set the limit to go between -2 and 24, set the ticks of the graph to be = to 3
     plt.xticks(range(0, 25, 3))
     plt.xlim(-2,24)
 
@@ -82,15 +102,32 @@ def createSingleDayHisto(date, data):
     plt.show()
 
 def main():
+    #main script
+
+    #create variables
     data, plottingdata, years = creatingVariables()
+
+    #creating maxkps
     max_kps = maxkpsCreation(data, years)
+
+    #printing statements for questions / debugging
     printstatements(plottingdata, data)
-    #firstGraph(plottingdata)
-    #secondGraph(max_kps, years)
+
+    #histogram graph
+    firstGraph(plottingdata)
+
+    #line graph
+    secondGraph(max_kps, years)
+
+    #siginificant years creation
     significantYears(data)
+
+    #dates to make bar charts out
     dates = [[1972,8,5], [2024,5,11]]
+
+    #for loop to make 2 bar charts
     for date in dates:
-        createSingleDayHisto(date, data)
+        createSingleDaybar(date, data)
     
 
 
